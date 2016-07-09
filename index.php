be21455d7828af59f898ac3abc83d5df1cfa04fc
aver803bath5 <aver803bath261@gmail.com>
2016-07-09 10:34:23 +0800

    add ppt and youtube

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="./font-awesome/css/font-awesome.min.css">
 	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 	<script src="./js/jquery-3.0.0.min.js"></script>
+	<script src="./js/jquery.cycle.all.js"></script>
 	<script src="./js/all.js"></script>
 </head>
 <body>
@@ -155,11 +156,38 @@
 			<a href="https://www.facebook.com/J.coating" class="facebooklink" target="_blank
 			"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
 			<div class="clear"></div>
-
 		</div>
 		<div class="whiteline"></div>
-		<div class="content"></div>
-		<div class="footer"></div>
+		<div class="content">
+			<div class="illustration">
+				<p>
+					各位愛車一族的朋友，歡迎各位參觀敝人的網站，本身畢業於中興大學精密工程所， 指導教授 劉柏良老師，對奈米科學材料還算小有研究，網站的文章都是小的我辛辛苦苦尋找科學期刊所得到的數據與資料，希望可以促進各位瞭解車體鍍膜到底為何?也希望同業請勿任意盜拷文章，畢竟找資料寫文章非常勞心勞力，感謝各位合作...^_^。
+
+				</p>
+				<a href="http://web.nchu.edu.tw/~pliu/nanosci/Welcome.html" class="nano" target="_blank">奈米科學計算實驗室</a>
+			</div>
+
+			<div class="ppt" id="ppt">
+				<img src="./images/297145.jpg" alt="">
+				<img src="./images/311809.jpg" alt="">
+				<img src="./images/300510.jpg" alt="">
+				<img src="./images/291984.jpg" alt="">
+			</div>
+
+			<div class="youtube">
+				<iframe width="1003px" height="543px" src="https://www.youtube.com/embed/ekbtbMS0AzY" frameborder="0" allowfullscreen></iframe>
+				<p>
+					匠 Coating車體鍍膜日本類玻璃鍍膜撥水測試，想必有在FOLLOW我們網站的各位，對這輛黑色SCRV應該不陌生吧!這是本店第一輛施工日本類玻璃鍍膜stage1+stage2雙劑結晶型鍍膜的車輛，車體鍍膜施工日期為2014年3月13日，昨天車主也回廠來自己DIY洗車，順便讓我們檢是鍍膜塗層的狀況，依照慣例我們仍交由車主親自洗車親自測試，避免我們動過手腳有作弊嫌疑，目前此車的日本類玻璃鍍膜施工已超過13個月，車主自從做過日本類玻璃鍍膜後，從未打過一次車蠟，單純只是到自助洗車場泡沫洗車，車主從事業務性質工作，用車頻率頗高，住家有地下停車場，上班地點則是路邊停車環境，平均一個月自行到自助洗車場泡沫洗車，日本類玻璃(聚硅氧烷)擁有極低的電絕緣介電常數約0.9F/m，可減低車輛行駛中因與空氣摩擦所產生的電荷交換，造成車身靜電導致惹塵，所以車身具有自潔(self-clean)能力，既然不輕易惹塵洗車頻率自然降低，各位可以看看13個月後的日本類玻璃鍍膜撥水效果如何，影片中的撥水效果也許有人會認為純屬特例，畢竟每個人對於愛車程度都不盡相同，使用車輛方式與維護方式也不相同，就正因為如此我們才要教育顧客車體鍍膜後應如何正確維護，讓各位消費者自己可以用最簡單迅速的方式來維護，只要懂得鍍膜自我維護相信各位的鍍膜也可以如同影片中那位車主的SCRV一樣耐久，我們與車主也將會持續追蹤此車日本類玻璃鍍膜(聚硅氧烷)到底可以維持多久而不用鍍膜維護，各位敬請期待此車半年後的測試。
+				</p>
+			</div>
+
+			<div class="information">
+				
+			</div>
+		</div>
+		<div class="footer">
+			
+		</div>
 	</div>
 </body>
 </html>
